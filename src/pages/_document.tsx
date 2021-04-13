bb9afa2fbe497162e89887ec5fa229ecdb6a3854
Johnny <jcarreiropublicidade@gmail.com>
2021-04-12 21:29:09 -0300

    Fonts were added to main document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+export default class Mydocument extends Document{
+  render():JSX.Element{
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet" />
+          <title>My Blog</title>
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+      )
+    }
+  }
