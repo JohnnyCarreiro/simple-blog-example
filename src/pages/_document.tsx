fdb3c24d712027194e39ef367816ce9d09826d43
Johnny <jcarreiropublicidade@gmail.com>
2021-04-12 23:27:44 -0300

    feat: Create Heder/nav compponent

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class Mydocument extends Document{
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+          <meta name="theme-color" content="#673AB6" />
         </Head>
         <body>
           <Main/>
