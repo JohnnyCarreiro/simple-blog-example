0015186c9ad6a563882ec67d22188c490536b2f5
Johnny <jcarreiropublicidade@gmail.com>
2021-04-12 22:17:28 -0300

    fix: Remove module style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
 
 const IndexPage = () => (
   <div>
     <Head>
       <title>Home | My Blog</title>
     </Head>
-    <h1
-      className={styles.title}
-    >
+    <h1>
       hello world 👋🏻 🌎
     </h1>
   </div>
