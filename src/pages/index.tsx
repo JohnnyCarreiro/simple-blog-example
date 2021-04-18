f4738d567c576d7b23c9822fd4b77b7e63df1761
Johnny <jcarreiropublicidade@gmail.com>
2021-04-18 20:00:16 -0300

    feat: Create subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 
 import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 const IndexPage = () => (
   <div>
@@ -16,6 +17,7 @@ const IndexPage = () => (
           Get access to all publications <br/>
           <span>for only $9.90 month</span>
         </p>
+        <SubscribeButton />
       </section>
       <img src="/images/avatar.svg" alt="Girl codding"/>
     </main>
